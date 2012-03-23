8a1f320963fb239c3c22275f6e911c7f2b5667bc
Rajat Kumar <rajat@ubuntu.(none)>
2012-03-22 20:45:45 -0400

    is_variable_unate and is_cover_unate changed

--- is_variable_unate.h
+++ is_variable_unate.h
@@ -3,32 +3,30 @@
 #include <string>
 using namespace std;
 
-bool variable_unate (char **c, int dim1, int dim2) {
+bool is_variable_unate (char **c, int dim1, int dim2, int col) {
 
-bool result = false;
+	bool result = false;
+	char tmp = '-'; //stores first encounted non DC value of column
 
-int j_c='0';
-for (int j=0; j<dim2; j++) {			//dims2=cols=no_of_inp
-
-	result = true;
-	//start row iterations from row 2
-	for (int i=1; i<dim1; i++) {		//dim1=rowno
-
-	cout<<"accessing:"<<c[i][j_c]<<endl;
-	cout<<"value of i:"<<i<<"value of j:"<<j<<endl;
-	
-		if (c[i][j_c] != c[i-1][j_c] && c[i][j_c]!='-' && c[i-1][j_c]!='-') {
-		result = false;
-		cout<<"cover is not unate in variable x"<<j<<endl;
-		break;      //break since change from 0 to 1 or vice versa has happened
+	//store first non DC value of input column
+	for ( int r = 0; r<dim1; r++) {
+		if (c[r][col] != '-') {
+			tmp = c[r][col];
+			break;
 		}
-}//loop over rows ends here
-
-j_c+=1;
-	if (result ==true){
-		cout<<"cover is unate in variable x"<<j<<endl;
 	}
-}
-
-return result;
+	//if all values DC
+	if (tmp == '-') {
+		result = true;
+	}
+	else {
+		//start row iterations from row 2
+		for (int i=1; i<dim1; i++) {		//dim1=rowno
+			if (c[i][col] != '-' && c[i][col] != tmp)
+				result = false;
+			else 
+				result = true;
+		} //loop over rows ends here
+	}
+	return result;
 }
